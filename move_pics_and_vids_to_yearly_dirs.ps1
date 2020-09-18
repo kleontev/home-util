957b2913e40e91ba814e05ebc6d15c179da84c57
kleontev <kleontev@users.noreply.github.com>
2020-09-18 22:42:34 +0700

    Create move_pics_and_vids_to_yearly_dirs.ps1

---
+++ move_pics_and_vids_to_yearly_dirs.ps1
@@ -0,0 +1,16 @@
+﻿# move phone image/video uploads to corresponding yearly folders
+
+$pic_dir = "$env:OneDrive\Pictures"
+$vid_dir = "$env:OneDrive\Videos"
+
+$files = "Samsung Gallery", "Camera Roll" | % { ls $pic_dir\$_ -Recurse -File }
+
+$files | % { $_.LastWriteTime.Year } | select -Unique | % {
+    mkdir $pic_dir\$_ -Force
+    mkdir $vid_dir\$_ -Force
+}
+
+foreach ($file in $files) {
+    $target_dir = if ($file.Extension -eq '.mp4') { $vid_dir } else { $pic_dir }    
+    mv $file.FullName $target_dir\$($_.LastWriteTime.Year) -Force
+}
